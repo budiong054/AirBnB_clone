17b8a8855f0f370bc8d6e64f0209d47cf7d6cd6c
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-11-24 10:25:38 +0000

    Implement the quit and EOF to exit the program

--- console.py
+++ console.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
-"""This defines the Airbnb console using cmd serving as frontend (for interaction)."""
+"""This defines the Airbnb console using cmd serving as frontend
+    (for interaction)
+"""
 import cmd
-import re
-from shlex import split
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
 
 
+class HBNBCommand(cmd.Cmd):
+    """This defines the Airbnb console inherited from the base class
+        cmd.Cmd
+    """
+    prompt = '(hbnb) '
 
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to signal end of file
+        """
+        return True
+
+    def emptyline(self):
+        """Execute nothing
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
